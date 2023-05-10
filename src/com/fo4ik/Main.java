a864ac46a733e316886bf62da2bc79811e3da11e
Vlad <zinkovskyi.vladyslav@gmail.com>
2023-05-10 12:30:24 +0200

    modify main

--- src/com/fo4ik/Main.java
+++ src/com/fo4ik/Main.java
@@ -14,13 +14,7 @@ public class Main {
     public static void main(String[] args) {
         FlatLightLaf.setup();
 
-        try{
-            UIManager.setLookAndFeel( new FlatLightLaf() );
-        } catch (Exception e) {
-            System.out.println("Error set look and feel");
-        }
         Locale locale = Locale.getDefault();
-        //Locale locale = new Locale("ru");
         Locale.setDefault(locale);
 
         FileProperties fileProperties = new FileProperties();
